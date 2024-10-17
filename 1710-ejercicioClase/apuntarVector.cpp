15870ad76f18df19a9f1f47f2474695fbbbf0749
ffrancoreyes <ffrancoreyes@frba.utn.edu.ar>
2024-10-17 11:32:14 -0300

    Ejercicios en clase 2da parte

--- 1710-ejercicioClase/apuntarVector.cpp
+++ 1710-ejercicioClase/apuntarVector.cpp
@@ -2,17 +2,15 @@
 using namespace std;
 
 int main(){
-    double vec[4] = {11,24,232,412};
-    double *punteroVec;
+    /*lo que hacemos primero es declarar nuestro vector con 4 valores. */
+    int vec[4] = {11,24,232,412};
+    //inicializamos el puntero para poder acceder luego a su ubicación en memora
+    int *punteroVec;
     punteroVec = &vec[0];
+    //imprimimos el valor, la ubicación del puntero
     for (int i = 0; i<4; i++){
-        cout << "ubicacion de " << vec[i] << " es: " << &vec[i] << " " << punteroVec+i << endl;
+        cout << "ubicacion de " << vec[i] << " es: " << &vec[i] << " " << *(punteroVec+i) << endl;
     }
-    cout << *(punteroVec+2);
-
-    //debemos resolver esta parte porque solo apunta a la misma dirección, a menos que todo el array se guarde en el mismo lugar, andamos troleando en eso
-    //Le preguntamos de ultima, pero mepa que el array guarda una sola direccion
-    //mepa que si, pero ante la duda la mas 
     
     return 0;
 
